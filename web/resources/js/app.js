dfc10f0e5ae8ad591ee050ad976818623dd5cf6d
shiva <shiva.seelam@gmail.com>
2017-06-02 18:37:38 +0530

    app js

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,9 +1,31 @@
 
 'use strict';
 
-var App = angular.module("ppolApp",[]);
+/*var App = angular.module("ppolApp",[]);*/
 
-var app = angular.module('LoginForm',['ngMaterial','ngAnimate','ngAria','ngMessages']);
+//var app = angular.module('LoginForm',['ngMaterial','ngAnimate','ngAria','ngMessages']);
+
+var  loginApp =angular.module('LoginForm', ['ngRoute','ngMaterial','ngAnimate','ngAria','ngMessages']);
+
+
+
+loginApp.config( function($routeProvider,$locationProvider) {
+         $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/Login',{
+
+                templateUrl: '/html/error.html'
+
+            },function(){console.log("routes")})
+        /*.otherwise({ redirectTo: '/html/error.html' });*/
+   /* $locationProvider.html5Mode({
+        enabled: true,
+
+    });*/
+        console.log("logins");
+    });
+
+
 
 
 
