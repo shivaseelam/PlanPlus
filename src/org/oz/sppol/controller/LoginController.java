40360a0f3ea8c62f84c4da477472998805d8de92
shivaseelam <shiva.seelam@gmail.com>
2017-06-05 13:25:32 +0530

    Update LoginController.java
    added login code

--- src/org/oz/sppol/controller/LoginController.java
+++ src/org/oz/sppol/controller/LoginController.java
@@ -2,13 +2,19 @@ package org.oz.sppol.controller;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.oz.sppol.dto.customerdto.CustomerDTO;
 import org.oz.sppol.service.common.subscriber.FwkSubscriberService;
 import org.oz.sppol.service.common.user.FwkUserService;
 import org.oz.sppol.service.common.user.FwkUserServiceImpl;
+import org.oz.sppol.service.customerservice.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,11 +23,15 @@ import java.util.Map;
 @RestController
 public class LoginController {
 
+
+
+    @Autowired
+    private CustomerService customerService;
    /* @RequestMapping(value="/login")
     public String showLoginForm(){
 
-        //return "redirect:/pages/login.html";
-        return "resources/login.html";
+        //return "redirect:/html/login.html";
+
     }*/
 
     @Autowired
@@ -32,30 +42,30 @@ public class LoginController {
     @RequestMapping(value="/login")
     public ModelAndView showLoginForm(){
 
-        //return "redirect:/pages/login.html";
-        return new ModelAndView("redirect:/pages/login.html");
+        return new ModelAndView("redirect:/html/login.html");
     }
-    @RequestMapping(value="/pages/submitform",method = RequestMethod.POST)
+    @RequestMapping(value="/html/submitform",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     //public  ModelAndView loginAuthenticate(@PathVariable("accountname") String accountname,@PathVariable("username")String username,@PathVariable("password") String password)
-    public  @ResponseBody String loginAuthenticate(@RequestBody Map data)
+    public ResponseEntity<List<CustomerDTO>> loginAuthenticate(@RequestBody Map data)
     {
 
-        JSONObject jsonData = new JSONObject();
-        JSONArray dataArray = new JSONArray();
+
 
         String accountname= (String) data.get("accountname");
         String username= (String) data.get("username");
         String password= (String) data.get("password");
         boolean msg = fwkSubscriberService.subscriberServiceExist("ahsWebBean",accountname);
-        if( fwkUserService.validate("ahsWebBean",username, password, "") >0)
-        {
-            dataArray.add(jsonData.put("done","success"));
-            return  dataArray.toJSONString();
-        }
-        dataArray.add(jsonData.put("msg","invalid"));
-        return  dataArray.toJSONString();
 
-        //return new ModelAndView("redirect:/customer_ng");
+        List<CustomerDTO> customerDTO = null;
+        customerDTO=customerService.getAllCustomersRest();
+        if(customerDTO  == null && fwkUserService.validate("ahsWebBean",username, password, "") >0)
+        {
+
+            return new ResponseEntity<List<CustomerDTO>>(HttpStatus.NO_CONTENT);
+        }
+    return new ResponseEntity<List<CustomerDTO>>(customerDTO, HttpStatus.OK);
+
+
     }
 
 }
